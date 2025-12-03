f7b207e2908e8d331d3c1bc3253be25a0ada22ea
blatantspace <blatantspace@gmail.com>
2025-12-03 07:35:40 -0600

    chore: remove unused components to fix build

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,2 @@
-export { default as DropZone } from './DropZone';
-export { default as ParameterPanel } from './ParameterPanel';
-export { default as ResultViewer } from './ResultViewer';
-export { default as StatusIndicator } from './StatusIndicator';
-
+export { default as ImageViewer } from './ImageViewer';
+export { default as SetupGuide } from './SetupGuide';
